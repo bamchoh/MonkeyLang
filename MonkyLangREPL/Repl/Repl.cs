cfa7d84d323f8418c7c233029537ff0c20f2d6cd
bamchoh <bamchoh@gmail.com>
2020-08-02 02:53:45 +0900

    2.9 update REPL to display error messages

--- MonkyLangREPL/Repl/Repl.cs
+++ MonkyLangREPL/Repl/Repl.cs
@@ -4,12 +4,27 @@ using System.Text;
 using System.IO;
 using MonkeyLang.Lexer;
 using MonkeyLang.Token;
+using MonkeyLang.Parser;
 
 namespace MonkyLangREPL.Repl
 {
     class Repl
     {
         static readonly string PROMPT = ">> ";
+
+        const string MONKEY_FACE = @"
+           --,--
+  .--.  .-'     '-.  .--.
+ / .. \/  .-. .-.  \/ .. \
+| |  '|  /   Y   \  |'  | |
+| \   \  \ 0 | 0 /  /'  / |
+ \ '- ,\.-^^^^^^^-./, -' /
+  ''-' /_   ^ ^   _\ '-''
+      |   \._ _./   |
+      \   \ '~' /   /
+       '._ '-=-' _.'
+          '-----'
+";
 
         public static void Start(TextReader tr, TextWriter tw)
         {
@@ -22,11 +37,28 @@ namespace MonkyLangREPL.Repl
                 }
 
                 var l = new Lexer(line);
+                var p = new Parser(l);
 
-                for(var tok = l.NextToken(); tok.Type != TokenTypes.EOF; tok = l.NextToken())
+                var program = p.ParserProgram();
+                if(p.Errors().Count != 0)
                 {
-                    Console.WriteLine("Type:{0}, Literal:{1}", tok.Type, tok.Literal);
+                    printParserErrors(tw, p.Errors());
+                    continue;
                 }
+
+                tw.Write(program.String());
+                tw.WriteLine();
+            }
+        }
+
+        private static void printParserErrors(TextWriter tw, List<string> errors)
+        {
+            tw.Write(MONKEY_FACE);
+            tw.WriteLine("Woops! We ran into some monkey business here!");
+            tw.WriteLine(" parser errors:");
+            foreach(var msg in errors)
+            {
+                tw.WriteLine(string.Format("\t{0}", msg));
             }
         }
     }
